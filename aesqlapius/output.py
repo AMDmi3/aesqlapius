67b4212dd7c3d4cc7bd1001fff08bbc7f9609731
Dmitry Marakasov <amdmi3@amdmi3.ru>
2020-11-23 03:48:04 +0300

    Add test for aesqlapius.output
    Make stack depth used by process_row() for custom type to extract
    type from customizable to simplify testing

--- aesqlapius/output.py
+++ aesqlapius/output.py
@@ -2,14 +2,15 @@ from typing import Any, Callable, Dict, List, Tuple, Union
 
 from aesqlapius.function_def import ReturnValueInnerFormat
 
-def generate_row_processor(inner_format: Union[ReturnValueInnerFormat, str], field_names: List[str]) -> Callable[[Tuple[Any]], Any]:
+def generate_row_processor(inner_format: Union[ReturnValueInnerFormat, str], field_names: List[str], stack_depth: int = 2) -> Callable[[Tuple[Any]], Any]:
     if inner_format is not ReturnValueInnerFormat:
         def process_row(row: Tuple[Any]) -> Dict[str, Any]:
-            # get the frame of method caller
+            # get the frame of method caller; usually,
             # [0] is process_row
             # [1] is generated method
             # [2] is caller
-            caller_frame = inspect.stack()[2].frame
+            # but stack depth is customizable to simplify testing the function
+            caller_frame = inspect.stack()[stack_depth].frame
 
             if inner_format in caller_frame.f_locals:
                 custom_format = caller_frame.f_locals[inner_format]
---
+++ tests/test_output.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+
+import pytest
+
+from aesqlapius.function_def import ReturnValueInnerFormat
+from aesqlapius.output import generate_row_processor
+
+
+@pytest.fixture
+def row():
+    return (1, 2, 3)
+
+
+@pytest.fixture
+def field_names():
+    return ['a', 'b', 'c']
+
+
+def test_tuple(row, field_names):
+    rp = generate_row_processor(ReturnValueInnerFormat.TUPLE, field_names)
+
+    assert rp(row) == (1, 2, 3)
+
+
+def test_dict(row, field_names):
+    rp = generate_row_processor(ReturnValueInnerFormat.DICT, field_names)
+
+    assert rp(row) == {'a': 1, 'b': 2, 'c': 3}
+
+
+def test_list(row, field_names):
+    rp = generate_row_processor(ReturnValueInnerFormat.LIST, field_names)
+
+    assert rp(row) == [1, 2, 3]
+
+
+def test_custom_local(row, field_names):
+    @dataclass
+    class MyLocalType:
+        a: int
+        b: int
+        c: int
+
+    rp = generate_row_processor('MyLocalType', field_names, 1)
+
+    assert rp(row) == MyLocalType(1, 2, 3)
+
+
+@dataclass
+class MyGlobalType:
+    a: int
+    b: int
+    c: int
+
+
+def test_custom_global(row, field_names):
+    rp = generate_row_processor('MyGlobalType', field_names, 1)
+
+    assert rp(row) == MyGlobalType(1, 2, 3)
