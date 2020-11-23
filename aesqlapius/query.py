a6ca14f70971cf3ecf60262a2248c467d1064cc3
Dmitry Marakasov <amdmi3@amdmi3.ru>
2020-11-23 04:08:43 +0300

    Remove dead code

--- aesqlapius/query.py
+++ aesqlapius/query.py
@@ -8,26 +8,6 @@ class Query:
     func_def: FunctionDefinition
     text: str
 
-    def prepare_args(self, *args, **kwargs) -> Dict[str, Any]:
-        prepared_args = {}
-
-        for narg, arg in enumerate(self.func_def.args):
-            value: Any = None
-            if narg < args.length():
-                if arg.name in kwargs:
-                    raise TypeError(f"{self.func_def.name} got multiple values for argument '{arg.name}'")
-                value = args[narg]
-            elif arg.name in kwargs:
-                value = kwargs[arg.name]
-            elif arg.has_default:
-                value = arg.default
-            else:
-                raise TypeError(f"{self.func_def.name} missing required positional argument '{arg.name}'")
-
-            prepared_args[name] = value
-
-        return prepared_args
-
 
 def parse_query(path: str) -> Query:
     func_def: Optional[FunctionDefinition] = None
