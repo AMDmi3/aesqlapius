16111b1f8c7b7a291258c46cbc4f3017dbc36041
Dmitry Marakasov <amdmi3@amdmi3.ru>
2022-11-07 14:15:37 +0300

    Update python versions

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: SQL',
         'Topic :: Database',
     ],
